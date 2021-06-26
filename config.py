16cd0c11e2e2647302eb502ea0eb6628a6d5a756
Максим Напрейкин <nikierpan@MBP-Maksim.mshome.net>
2021-06-26 10:39:45 +0300

    Setup configs in config.py

---
+++ config.py
@@ -0,0 +1,45 @@
+from os import environ
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent
+
+
+class Config:
+    SECRET_KEY = environ.get('SECRET_KEY')
+
+    MAIL_SERVER = environ.get('MAIL_SERVER', 'smtp.mail.ru')
+    MAIL_PORT = int(environ.get('MAIL_PORT', '465'))
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
+    MAIL_SUBJECT_PREFIX = environ.get('MAIL_SUBJECT_PREFIX')
+    MAIL_SENDER = environ.get('MAIL_SENDER')
+
+    ADMIN = environ.get('ADMIN')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = environ.get('DEV_DATABASE_URL')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = environ.get('TEST_DATABASE_URL')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
