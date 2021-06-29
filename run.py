bdef1aed1e6c83529379786e3a82a53203180c73
Максим Напрейкин <nikierpan@MacBook-Pro-Maksim.local>
2021-06-29 17:13:43 +0300

    Connect dev and test DB and add User model

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from os import getenv
 from pathlib import Path
 from dotenv import load_dotenv
+from flask_migrate import Migrate
 
-from app import create_app
+from app import create_app, db
+from app.models import User
 
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -11,3 +13,9 @@ if Path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
 app = create_app(getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User)
